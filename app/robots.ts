55c5a24157c69fc93c9568076bbbac79310e255b
Percy-Mic <percymicnono@gmail.com>
2026-09-26 09:44:29 +0800

    seo: allow public feed crawling

--- app/robots.ts
+++ app/robots.ts
@@ -10,7 +10,6 @@ export default function robots(): MetadataRoute.Robots {
         allow: '/',
         disallow: [
           '/dashboard',
-          '/feed',
           '/messages',
           '/settings',
           '/notes/',
